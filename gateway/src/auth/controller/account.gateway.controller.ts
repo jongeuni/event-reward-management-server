9de9e8448413a698ce1ff62cb76a667296408726
jongeuni <lje0818ovo@naver.com>
2025-05-16 16:26:19 +0900

    #3 feat: 게이트웨이 로그인 요청 추가

--- gateway/src/auth/controller/account.gateway.controller.ts
+++ gateway/src/auth/controller/account.gateway.controller.ts
@@ -3,15 +3,25 @@ import { firstValueFrom } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
 import { SignUpRq } from './rqrs/sign-up.rq';
 import { SignUpRs } from './rqrs/sign-up.rs';
+import { SignInRq } from './rqrs/sign-in.rq';
+import { SignInRs } from './rqrs/sign-in.rs';
 
-@Controller('/v1/accounts')
+@Controller('/v1')
 export class AccountGatewayController {
   constructor(private readonly httpService: HttpService) {}
 
-  @Post()
-  async handleUserRequests(@Body() rq: SignUpRq): Promise<SignUpRs> {
+  @Post('/sign-up')
+  async signUp(@Body() rq: SignUpRq): Promise<SignUpRs> {
     const response = await firstValueFrom(
       this.httpService.post<SignUpRs>('http://127.0.0.1:3001/sign-up', rq),
+    );
+    return response.data;
+  }
+
+  @Post('/sign-in')
+  async signIn(@Body() rq: SignInRq): Promise<SignInRs> {
+    const response = await firstValueFrom(
+      this.httpService.post<SignInRs>('http://127.0.0.1:3001/sign-in', rq),
     );
     return response.data;
   }
