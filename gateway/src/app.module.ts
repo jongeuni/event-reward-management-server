9473cad9c13186932c3be5f556a66630c27cd38c
jongeuni <lje0818ovo@naver.com>
2025-05-20 22:01:31 +0900

    #19 fix: health 처리 수정 및 추가

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthServerGatewayModule } from './common/module/auth-server.gateway.module';
 import { EventServerGatewayModule } from './common/module/event-server.gateway.module';
 import { AuthModule } from './common/auth/auth.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [AuthServerGatewayModule, EventServerGatewayModule, AuthModule],
+  controllers: [AppController],
 })
 export class AppModule {}
